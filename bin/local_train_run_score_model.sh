ea4c51ec41ef1118e02ce1fdb73f710f83a4f5ff
Bartosz Puszkarski <bartosz.puszkarski@addepto.com>
2025-07-05 08:39:12 +0200

    dir paths for models

--- bin/local_train_run_score_model.sh
+++ bin/local_train_run_score_model.sh
@@ -4,6 +4,6 @@ echo $LOG_FILE_NAME
 mkdir -p models/$BRANCH_NAME
 mkdir -p ../local_test_outputs/$BRANCH_NAME
 
-nohup bash -c 'python train_model.py -d ../training_data/all_files/ -m models/$BRANCH_NAME ;python run_model.py -d ../test_files/ -m models/$BRANCH_NAME -o ../local_test_outputs/$BRANCH_NAME/ ;python evaluate_model.py -d ../test_files/ -o ../local_test_outputs/$BRANCH_NAME/ ' >> $LOG_FILE_NAME & 
+nohup bash -c 'python train_model.py -d ../training_data/all_files/ -m models/$BRANCH_NAME/ ;python run_model.py -d ../test_files/ -m models/$BRANCH_NAME/ -o ../local_test_outputs/$BRANCH_NAME/ ;python evaluate_model.py -d ../test_files/ -o ../local_test_outputs/$BRANCH_NAME/ ' >> $LOG_FILE_NAME & 
 
 echo "$(date)" >> $LOG_FILE_NAME 
