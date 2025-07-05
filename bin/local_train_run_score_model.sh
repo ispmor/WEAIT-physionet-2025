3ee3a0b0626fb15c9d2a78cdb89673fad8a68909
Bartosz Puszkarski <bartosz.puszkarski@addepto.com>
2025-07-05 08:44:56 +0200

    adjustments to script and channels shift

--- bin/local_train_run_score_model.sh
+++ bin/local_train_run_score_model.sh
@@ -1,9 +1,9 @@
-BRANCH_NAME=$(git symbolic-ref --short HEAD | sed -r 's/\//_/g') 
-LOG_FILE_NAME="logs/$BRANCH_NAME-$(date +"%Y-%m-%d_%H:%M:%S")"
+export BRANCH_NAME=$(git symbolic-ref --short HEAD | sed -r 's/\//_/g') 
+export LOG_FILE_NAME="logs/$BRANCH_NAME-$(date +"%Y-%m-%d_%H:%M:%S")"
 echo $LOG_FILE_NAME
 mkdir -p models/$BRANCH_NAME
 mkdir -p ../local_test_outputs/$BRANCH_NAME
 
-nohup bash -c 'python train_model.py -d ../training_data/all_files/ -m models/$BRANCH_NAME/ ;python run_model.py -d ../test_files/ -m models/$BRANCH_NAME/ -o ../local_test_outputs/$BRANCH_NAME/ ;python evaluate_model.py -d ../test_files/ -o ../local_test_outputs/$BRANCH_NAME/ ' >> $LOG_FILE_NAME & 
+nohup bash -c "python train_model.py -d ../training_data/all_files/ -m models/$BRANCH_NAME/ ;python run_model.py -d ../test_files/ -m models/$BRANCH_NAME/ -o ../local_test_outputs/$BRANCH_NAME/ ;python evaluate_model.py -d ../test_files/ -o ../local_test_outputs/$BRANCH_NAME/" >> $LOG_FILE_NAME & 
 
 echo "$(date)" >> $LOG_FILE_NAME 
