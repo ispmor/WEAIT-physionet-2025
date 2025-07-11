86f1633a94c20ec215247f1c1c2b189cb7584939
Bartosz Puszkarski <bartosz.puszkarski@addepto.com>
2025-07-11 22:41:51 +0200

    36x8

--- config.py
+++ config.py
@@ -21,10 +21,10 @@ class ARGS():
         self.fold_to_process = ""
         self.network = "LSTM"
         self.include_domain = False
-        self.alpha_hidden=72
-        self.alpha_layers=4
-        self.beta_hidden=72
-        self.beta_layers=4
+        self.alpha_hidden=36
+        self.alpha_layers=8
+        self.beta_hidden=36
+        self.beta_layers=8
         self.epochs=30
         self.early_stop=6
         self.fold = 1
