495a19d848284ed09a2e198f5149f9b4d48d6b52
Bartosz Puszkarski <bartosz.puszkarski@addepto.com>
2025-08-07 15:42:32 +0200

    run in background

--- bin/local_train_only.sh
+++ bin/local_train_only.sh
@@ -4,5 +4,5 @@ echo $LOG_FILE_NAME
 mkdir -p models/$BRANCH_NAME
 mkdir -p ../local_test_outputs/$BRANCH_NAME
 
-nohup bash -c "python train_model.py -d ../training_data/all_files/ -m models/$BRANCH_NAME/ ;"
+nohup bash -c "python train_model.py -d ../training_data/all_files/ -m models/$BRANCH_NAME/ ;" &
 echo "$(date)" >> $LOG_FILE_NAME 
